b03021a4d73f65353cf03d3ea678fda34c4cf7fc
Jorge Calderón <jcalderon900610@gmail.com>
2025-08-12 19:54:01 -0400

    CORS

--- src/app.js
+++ src/app.js
@@ -14,8 +14,8 @@ const app = express();
 
 app.use(cors({
 
-    // origin: 'http://localhost:5173',
-    origin: '*',
+    origin: 'http://localhost:5175',
+    // origin: '*',
     credentials: true, // ¡Importante!
 
 }));
